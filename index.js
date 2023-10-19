26495c3e7c949024c1b2ee8b1fd176ec5cd3e8ea
confleux <confleux@gmail.com>
2023-10-19 18:10:24 +0300

    chore: restore default logger

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const readline = require('readline/promises');
 const { stdin: input, stdout: output } = require('process');
 const config = require('./lib/configReader');
+const logger = require('./lib/log');
 const { processFile } = require('./lib/processFile');
 const { sendTokens } = require('./lib/sendTokens');
 
 async function main() {
   const rl = readline.createInterface({ input, output });
-  const answer = await rl.question(`Start airdrop? (type 'yes' to start): \n`);
+  const answer = await rl.question(`ATTENTION! \nStart airdrop? \nPlease, check your config once again \nType 'yes' to start: \n`);
 
   if (answer !== 'yes') {
     process.exit(1);
@@ -14,7 +15,11 @@ async function main() {
 
   const { validAddresses, invalidAddresses } = await processFile(config.inputFile);
 
-  const { successfulAddresses, failedAddresses } = await sendTokens(validAddresses);
+  logger.log(`Successfully processed input file`);
+  logger.log(`Valid addresses: ${validAddresses.length}`);
+  logger.warn(`Invalid addresses: ${invalidAddresses.length}`);
+
+  //const { successfulAddresses, failedAddresses } = await sendTokens(validAddresses);
 }
 
 main();
