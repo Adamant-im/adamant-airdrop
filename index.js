21d3acc7a7611288f8d05e87899a2c64241257b9
confleux <confleux@gmail.com>
2023-10-19 18:02:36 +0300

    chore: add running protection

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const readline = require('readline/promises');
+const { stdin: input, stdout: output } = require('process');
 const config = require('./lib/configReader');
 const { processFile } = require('./lib/processFile');
 const { sendTokens } = require('./lib/sendTokens');
 
 async function main() {
+  const rl = readline.createInterface({ input, output });
+  const answer = await rl.question(`Start airdrop? (type 'yes' to start): \n`);
+
+  if (answer !== 'yes') {
+    process.exit(1);
+  }
+
   const { validAddresses, invalidAddresses } = await processFile(config.inputFile);
 
   const { successfulAddresses, failedAddresses } = await sendTokens(validAddresses);
