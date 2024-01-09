7073a2dbcf1612fd13f726958520cf261571558a
martiliones <hi@martilion.es>
2024-01-10 01:09:28 +0600

    refactor: remove unnecessary template literals

--- src/logger/output/utils.js
+++ src/logger/output/utils.js
@@ -1,11 +1,12 @@
 import fs from 'fs'
+
 import { config } from '../../config/index.js'
 import { fullDate } from '../../utils/date.js'
 
 export function createOutputDirectories() {
   const outputPath = `./${config.outputPath}/${fullDate()}`
 
-  createDirectory(`${outputPath}`)
+  createDirectory(outputPath)
 
   return outputPath
 }
